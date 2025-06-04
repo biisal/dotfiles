c75f6b94e862f8107c60ad55f56bba5154caa572
Avisek Ray <biisal.int@gmail.com>
2025-06-04 22:33:18 +0530

    added mini statusline and zsh upgradd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,72 +1,75 @@
 return {
-    {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-	    "saghen/blink.cmp",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+		},
+		config = function()
+			local capabilities = require('blink.cmp').get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
+			-- Lua LSP setup
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				filetypes = { "lua" },
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+
+			-- Pyright LSP setup with strict mode
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				filetypes = { "python" },
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "strict",
+						},
+					},
+				},
+			})
+			-- vtsls lsp
+			lspconfig.vtsls.setup({
+				capabilities = capabilities,
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "json", "html" },
+			})
+
+			-- html lsp
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+
+			-- css lsp
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+
+			-- tailwindcss lsp
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			-- gopls lsp
+			--			lspconfig.gopls.setup({
+			--				capabilities = capabilities,
+			--				filetypes = { "go" },
+			--				settings = {
+			--					gopls = {
+			--						analyses = {
+			--							unusedparams = true,
+			--						},
+			--						staticcheck = true,
+			--					},
+			--				},
+			--			})
+
+			-- svelte lsp
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+		end,
 	},
-	config = function()
-	    local capabilities = require('blink.cmp').get_lsp_capabilities()
-	    local lspconfig = require("lspconfig")
-	    -- Lua LSP setup
-	    lspconfig.lua_ls.setup({
-		capabilities = capabilities,
-		settings = {
-		    Lua = {
-			diagnostics = {
-			    globals = { "vim" },
-			},
-		    },
-		},
-	    })
-
-	    -- Pyright LSP setup with strict mode
-	    lspconfig.pyright.setup({
-		capabilities = capabilities,
-		settings = {
-		    python = {
-			analysis = {
-			    typeCheckingMode = "strict",
-			},
-		    },
-		},
-	    })
-	    -- vtsls lsp
-	    lspconfig.vtsls.setup({
-		capabilities = capabilities,
-		filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "json", "html" },
-	    })
-
-	    -- html lsp
-	    lspconfig.html.setup({
-		capabilities = capabilities,
-	    })
-
-	    -- css lsp
-	    lspconfig.cssls.setup({
-		capabilities = capabilities,
-	    })
-
-	    -- tailwindcss lsp
-	    lspconfig.tailwindcss.setup({
-		capabilities = capabilities,
-	    })
-	    -- gopls lsp
-	    lspconfig.gopls.setup({
-		capabilities = capabilities,
-		settings = {
-		    gopls = {
-			analyses = {
-			    unusedparams = true,
-			},
-			staticcheck = true,
-		    },
-		},
-	    })
-
-	    -- svelte lsp
-	    lspconfig.svelte.setup({
-		capabilities = capabilities,
-	    })
-	end,
-    },
 }
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,23 +1,25 @@
 return {
-    {
-	"williamboman/mason.nvim",
-	dependencies = {
-	    "williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-	    require("mason").setup()
-	    require("mason-lspconfig").setup {
-		ensure_installed = {
-		    "lua_ls",
-		    "pyright",
-		    "vtsls",
-		    "gopls",
-		    "svelte",
-		    "tailwindcss",
-		    "cssls",
-		    "html",
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
 		},
-	    }
-	end,
-    },
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup {
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"vtsls",
+					"gopls",
+					"svelte",
+					"tailwindcss",
+					"cssls",
+					"html",
+				},
+				handlers = {}, -- Disable automatic setup
+
+			}
+		end,
+	},
 }
